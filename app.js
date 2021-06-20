2789bcb9c02c490f3382cadeb8eaaa4583583be2
David Collins <davidc969@gmail.com>
2021-06-20 13:11:21 +0100

    adding checkbox slider and billing change functionality

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const app = Vue.createApp({
     data() {
       return {
         price: "16.00",
-        priceType: "Month",
         sliderVal: 50,
-        pageViews: "100k"
+        pageViews: "100k",
+        priceType: "Month",
+        billingMonthly: true
       };
     },
     methods: {
         sliderChange(){
-            if(this.priceType == "Month"){
+            if(this.billingMonthly == true){
                 if(inRange(this.sliderVal, 0, 20)){
                     this.price = "8.00",
                     this.pageViews = "10k"
@@ -31,9 +32,33 @@ const app = Vue.createApp({
                     this.pageViews = "1M"
                 } 
             } else {
-
+                if(inRange(this.sliderVal, 0, 20)){
+                    this.price = (8 * 12) * .25 + ".00",
+                    this.pageViews = "10k"
+                } else if(inRange(this.sliderVal, 21, 40)){
+                    this.price = (12 * 12) * .25 + ".00",
+                    this.pageViews = "50k"
+                } else if(inRange(this.sliderVal, 41, 60)){
+                    this.price = (16 * 12) * .25 + ".00",
+                    this.pageViews = "100k"
+                } else if(inRange(this.sliderVal, 61, 80)){
+                    this.price = (24 * 12) * .25 + ".00",
+                    this.pageViews = "500k"
+                } else if(inRange(this.sliderVal, 81, 100)){
+                    this.price = (36 * 12) * .25 + ".00",
+                    this.pageViews = "1M"
+                } 
             }
 
+        },
+        checkChange() {
+            this.billingMonthly = !this.billingMonthly
+            if (this.billingMonthly) {
+                this.priceType = "Month"
+            } else {
+                this.priceType = "Year"
+            }
+            this.sliderChange()
         }
     },
   });
